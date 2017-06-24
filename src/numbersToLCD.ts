d2d2b8fc334cbc633531bd2117b7064df1b71ba1
DIWithers <d.ivy.withers@gmail.com>
2017-06-24 10:18:08 -0400

    removed 2D array with more expressive parameter

--- src/numbersToLCD.ts
+++ src/numbersToLCD.ts
@@ -1,13 +1,13 @@
 export class NumbersToLCD {
-    static translate(input: number): string {
+    static translate(inputAsIndex: number): string {
         let translationArray: any = [
-            [0, " _ \n| |\n|_|"],
-            [1, "   \n  |\n  |" ],
-            [2, " _ \n _|\n|_ "],
-            [3, " _ \n _|\n _|"],
-            [4, "   \n|_|\n  |"],
-            [5, " _ \n|_ \n _|"]
+            " _ \n| |\n|_|",
+            "   \n  |\n  |",
+            " _ \n _|\n|_ ",
+            " _ \n _|\n _|",
+            "   \n|_|\n  |",
+            " _ \n|_ \n _|"
         ]
-        return translationArray[input][1];
+        return translationArray[inputAsIndex];
     }
 }
