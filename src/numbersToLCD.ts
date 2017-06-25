273f68cce68e4a7901c92d115eeeb287fdb8d31f
DIWithers <d.ivy.withers@gmail.com>
2017-06-25 08:55:34 -0400

    refactor to lcd pieces

--- src/numbersToLCD.ts
+++ src/numbersToLCD.ts
@@ -1,11 +1,21 @@
 export class NumbersToLCD {
 
     static translate(input: number): string {
+        let top: string = "";
+        let middle: string = "";
+        let bottom: string = "";
+
         if (input === 1) {
-            return "   \n  |\n  |";
+            top += "   ";
+            middle += "  |";
+            bottom += "  |";
+            return top + "\n" + middle + "\n" + bottom;
         }
         if (input === 2) {
-            return " _ \n _|\n|_ ";
+            top += " _ ";
+            middle += " _|";
+            bottom += "|_ ";
+            return top + "\n" + middle + "\n" + bottom;
         }
         else {
             return " _ \n| |\n|_|";
