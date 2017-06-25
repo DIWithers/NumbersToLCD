48765cad537df3ceae51527eb1057eaa0d51d102
DIWithers <d.ivy.withers@gmail.com>
2017-06-25 09:11:33 -0400

    Extracted lcd test

--- spec/numbersToLCD.spec.ts
+++ spec/numbersToLCD.spec.ts
@@ -1,16 +1,19 @@
 import {NumbersToLCD} from "../src/numbersToLCD";
 
 describe ("Numbers To LCD", () => {
+    function NumbersToLCDTest(input: number, expected: string): void {
+        expect(NumbersToLCD.translate(input)).toBe(expected);
+    }
  it("Should convert number 0 to LCD display of 0", () => {
-  expect(NumbersToLCD.translate(0)).toBe(" _ \n| |\n|_|");
+     NumbersToLCDTest(0, " _ \n| |\n|_|" );
  });
  it("Should convert number 1 to LCD display of 1", () => {
-  expect(NumbersToLCD.translate(1)).toBe("   \n  |\n  |");
+     NumbersToLCDTest(1, "   \n  |\n  |");
  });
  it("Should convert number 2 to LCD display of 2", () => {
-  expect(NumbersToLCD.translate(2)).toBe(" _ \n _|\n|_ ");
+     NumbersToLCDTest(2, " _ \n _|\n|_ ");
  });
  it("Should convert number 3 to LCD display of 3", () => {
-  expect(NumbersToLCD.translate(3)).toBe(" _ \n _|\n _|");
+     NumbersToLCDTest(3, " _ \n _|\n _|");
  });
 });
