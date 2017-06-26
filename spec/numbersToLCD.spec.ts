4f780a4b8c97dbfd50bf42e6f1e8a6025c59fa55
DIWithers <d.ivy.withers@gmail.com>
2017-06-26 07:35:01 -0400

    reset for third round

--- spec/numbersToLCD.spec.ts
+++ spec/numbersToLCD.spec.ts
@@ -1,44 +1,8 @@
-import {NumbersToLCD} from "../src/numbersToLCD";
 
-describe ("Numbers To LCD", () => {
-    function NumbersToLCDTest(input: number, expected: string): void {
-        expect(NumbersToLCD.translate(input)).toBe(expected);
-    }
- it("Should convert number 0 to LCD display of 0", () => {
-     NumbersToLCDTest(0, " _ \n| |\n|_|" );
+describe ("", () => {
+
+ it("", () => {
  });
- it("Should convert number 1 to LCD display of 1", () => {
-     NumbersToLCDTest(1, "   \n  |\n  |");
- });
- it("Should convert number 2 to LCD display of 2", () => {
-     NumbersToLCDTest(2, " _ \n _|\n|_ ");
- });
- it("Should convert number 3 to LCD display of 3", () => {
-     NumbersToLCDTest(3, " _ \n _|\n _|");
- });
- it("Should convert number 4 to LCD display of 4", () => {
-     NumbersToLCDTest(4, "   \n|_|\n  |");
- });
- it("Should convert number 5 to LCD display of 5", () => {
-     NumbersToLCDTest(5, " _ \n|_ \n _|");
- });
- it("Should convert number 6 to LCD display of 6", () => {
-     NumbersToLCDTest(6, " _ \n|_ \n|_|");
- });
- it("Should convert number 7 to LCD display of 7", () => {
-     NumbersToLCDTest(7, " _ \n  |\n  |");
- });
- it("Should convert number 8 to LCD display of 8", () => {
-     NumbersToLCDTest(8, " _ \n|_|\n|_|");
- });
- it("Should convert number 9 to LCD display of 9", () => {
-     NumbersToLCDTest(9, " _ \n|_|\n _|");
- });
- it("Should convert number 10 to LCD display of 10", () => {
-     NumbersToLCDTest(10, "    _ \n  || |\n  ||_|");
- });
- it("Should convert triple digit 134 to LCD display of 134", () => {
-     NumbersToLCDTest(134, "    _    \n  | _||_|\n  | _|  |");
- });
+
 
 });
